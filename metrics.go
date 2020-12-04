e19f447dab7b5b6e2414fbe0550034a051ab6027
Jordi Masip <jordi@masip.cat>
2020-12-04 17:22:50 +0100

    Change metrics names

--- metrics.go
+++ metrics.go
@@ -19,14 +19,14 @@ func define(subsystem string) {
 	requestCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: namespace,
 		Subsystem: subsystem,
-		Name:      "request_count_total",
+		Name:      "regex_request_count_total",
 		Help:      "Counter of HTTP(S) requests made.",
 	}, []string{"host", "path"})
 
 	requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: namespace,
 		Subsystem: subsystem,
-		Name:      "request_duration_seconds",
+		Name:      "regex_request_duration_seconds",
 		Help:      "Histogram of the time (in seconds) each request took.",
 		Buckets:   append(prometheus.DefBuckets, 30, 60),
 	}, []string{"host", "path", "status"})
@@ -34,7 +34,7 @@ func define(subsystem string) {
 	responseSize = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: namespace,
 		Subsystem: subsystem,
-		Name:      "response_size_bytes",
+		Name:      "regex_response_size_bytes",
 		Help:      "Size of the returns response in bytes.",
 		Buckets:   []float64{0, 1e3, 1e4, 1e5, 1e6, 5e6},
 	}, []string{"host", "path", "status"})
@@ -42,14 +42,14 @@ func define(subsystem string) {
 	responseStatus = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: namespace,
 		Subsystem: subsystem,
-		Name:      "response_status_count_total",
+		Name:      "regex_response_status_count_total",
 		Help:      "Counter of response status codes.",
 	}, []string{"host", "path", "status"})
 
 	responseLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: namespace,
 		Subsystem: subsystem,
-		Name:      "response_latency_seconds",
+		Name:      "regex_response_latency_seconds",
 		Help:      "Histogram of the time (in seconds) until the first write for each request.",
 		Buckets:   append(prometheus.DefBuckets, 30, 60),
 	}, []string{"host", "path", "status"})
